f62887d853c7d379649b40ad63e5082447b18c5c
윤여경/DS팀(YA) <yeokyeong.yoon@yanolja.com>
2023-08-20 20:08:37 +0900

    Add a test

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,7 +3,13 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
@@ -14,8 +20,30 @@ func Hello(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	// := operator is a shortcut for declaring and initializing a variable in one line
 	return message, nil /*nil (meaning no rror) as a second value in the successful return. That way, the caller can see that the function succeeded.
 	 */
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names { // '_' is the index of the current item
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
