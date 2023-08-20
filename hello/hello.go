3ef439d26de96ab1c75f5de24ed157bda2972b40
윤여경/DS팀(YA) <yeokyeong.yoon@yanolja.com>
2023-08-20 18:41:17 +0900

    return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -11,10 +11,12 @@ func main() {
 	log.SetPrefix("greetings : ")
 	log.SetFlags(0)
 
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
 	// Get a greeting message and print it.
-	message, err := greetings.Hello("Gladys")
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
